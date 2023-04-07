d6e616044d813b24d3293eae5cf105eabfdc927b
Johannes Becker <jaybee@sihlfall.ch>
2023-04-07 23:28:43 +0200

    Cntd

--- test/useRepreselector.spec.tsx
+++ test/useRepreselector.spec.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import assert from 'assert';
-import { render } from '@testing-library/react';
-import { act } from '@testing-library/react-hooks/dom';
-import { afterEach, beforeEach } from 'mocha';
-import { TestComponent } from './testComponent';
 import { createStore } from 'redux';
+import { TestComponent } from './testComponent';
 import { renderWithProviders } from './reduxUtil';
-import { reducer } from './testCoreRedux';
+import { INCREMENT_ACTION, reducer } from './testCoreRedux';
+import { act } from '@testing-library/react';
+import assert from 'assert';
 
 describe("useRepreselector", function () {
   it("does something", function () {
     const store = createStore(reducer);
     const { container } = renderWithProviders(<TestComponent />, store);
-    assert.deepStrictEqual(container?.textContent, "This is my test components.");
+    assert.deepStrictEqual(container?.textContent, "This is my test component. 101");
+    act( () => { store.dispatch({ type: INCREMENT_ACTION }) });
+    console.log(store.getState());
+    assert.deepStrictEqual(container?.textContent, "This is my test component. 102");
   });
 });
